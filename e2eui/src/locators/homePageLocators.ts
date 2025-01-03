c7cde42e66a041a1cfe9d07ce80c9ba8773f9d02
BLPSankalpana <88890398+BLPSankalpana@users.noreply.github.com>
2025-01-03 12:31:47 +0530

    update Select a Book and check if it views Book details

--- e2eui/src/locators/homePageLocators.ts
+++ e2eui/src/locators/homePageLocators.ts
@@ -1,9 +1,8 @@
 export class HomePageLocators{
   
     static readonly FilterTitle = '.mat-mdc-card-title.mat-h1';
-    static readonly BookCard = 'div.p-1';
+    static readonly BookCard = '.p-1:nth-child(n) > app-book-card';
     static readonly HomePageFirstAddToCartButton = '(//app-home//app-book-card//app-addtocart/button)[1]';
-    static readonly BookCardTitle = 'a strong';
-    static readonly BookCardPrice = 'p';
-    
+    static readonly BookCardTitle = 'div.card-title.my-2 a strong'; 
+    static readonly BookCardPrice = '.mat-mdc-card-content p';
 }
