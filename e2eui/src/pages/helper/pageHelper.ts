7e51a208926f29812aae3ba59dd2e94f559451b0
Kalana Gunathilaka <105417887+kalanagunathilaka@users.noreply.github.com>
2025-01-03 11:43:57 +0530

    add page Helper essential features and login features

--- e2eui/src/pages/helper/pageHelper.ts
+++ e2eui/src/pages/helper/pageHelper.ts
@@ -1,6 +1,8 @@
 import { Page } from 'playwright';
 import { DataFactory } from '../../utils/dataFactory';
 import { PlaywrightConfig } from './../../utils/playwrightConfig';
+import { Url } from '../../data/enum/Urls';
+import { HeaderLocators } from '../../locators/headerLocator';
 
 export class PageHelper {
     private page: Page = undefined as unknown as Page;
@@ -10,6 +12,36 @@ export class PageHelper {
     constructor() {
         this.playwrightConfig = PlaywrightConfig.getInstance();
         this.dataFactory = DataFactory.getInstance();
+    }
+
+    public async verifyUserIsLoggedIn(): Promise<boolean> {
+        this.page = await this.playwrightConfig.getPage();
+        const userName = this.dataFactory.getData().loginData.userDetails.userName;
+        
+        await this.urlNavigate(Url.BASEURL);
+
+        const loginVisible = await this.page.isVisible(HeaderLocators.LOGIN);
+        const usernameVisible = await this.page.isVisible(`${HeaderLocators.USERNAME}:has-text("${userName}")`);
+
+        return usernameVisible ? true : loginVisible ? false : (() => { throw new Error("Unable to determine user login status. Neither login nor username is visible."); })();
+    }
+
+    public async urlNavigate(turl: string): Promise<void> {
+        this.page = await this.playwrightConfig.getPage();
+    
+        // Normalize URLs to remove trailing slash for comparison
+        const currentUrl = this.page.url().replace(/\/$/, "");
+        const targetUrl = (turl === Url.BASEURL ? Url.BASEURL : Url.BASEURL + turl).replace(/\/$/, "");
+        
+        if (currentUrl !== targetUrl) {
+            //await this.page.goto(url);
+            // Use history.pushState to navigate without reloading the page
+            await this.page.evaluate((url) => {
+                window.history.pushState({}, "", url);
+                window.dispatchEvent(new PopStateEvent("popstate"));
+            }, turl);
+        }
+        await this.page.waitForTimeout(1000);
     }
 
     public async waitForApiResponse(apiEndpoint: string, expectedStatus: number = 200, timeout: number = 30000): Promise<any> {
